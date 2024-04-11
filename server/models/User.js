9f5510bb5ca5f93ed84c098f6ec05dc75bdf15e2
dymoy <148160802+dymoy@users.noreply.github.com>
2024-04-11 15:53:26 -0700

    Initial commit.

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  /* TODO: Sessions, Posts, Comments */
+  
+});
+
+/* Hash entered password before entering it into the db */
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+/* Helper function for password validation */
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
