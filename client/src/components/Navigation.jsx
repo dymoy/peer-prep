0d63fb893d062aa48cd153dd32089f97d5e207dd
mbullock710 <michael.bullock@guildeducation.com>
2024-04-19 09:20:41 -0500

    edited App.css so navbard will not intersect with html

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -6,8 +6,9 @@ import Login from "./Login";
 import Auth from "../utils/auth";
 import iconImage from "../../public/assets/pp_icon.jpg";
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@300..900&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-</style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@300..900&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+</style>;
 
 const Navigation = () => {
   const [showModal, setShowModal] = useState(false);
@@ -16,29 +17,31 @@ const Navigation = () => {
     <>
       <Navbar id="navigation-bar" expand="lg">
         <div id="icon-title-group">
-            <img id="icon" src={iconImage} alt="Icon" />
-            <h1  id="site-title">Peer Prep</h1>
-          </div>
-          <Navbar.Toggle aria-controls="navbar" />
-          <Navbar.Collapse id="navbar" className="justify-content-end">
+          <img id="icon" src={iconImage} alt="Icon" />
+          <h1 id="site-title">Peer Prep</h1>
+        </div>
+        <Navbar.Toggle aria-controls="navbar" />
+        <Navbar.Collapse id="navbar" className="justify-content-end">
           <Nav className="ml-auto d-flex">
-            <Nav.Link as={Link} to='/'>
-                  Explore Sessions
+            <Nav.Link as={Link} to="/">
+            <strong>Explore Sessions</strong>
+            </Nav.Link>
+            {Auth.loggedIn() ? (
+              <>
+                <Nav.Link as={Link} to="/mysessions">
+                  <strong>My Sessions</strong>
                 </Nav.Link>
-                {Auth.loggedIn() ? (
-                  <>
-                    <Nav.Link as={Link} to="/mysessions">
-                    <strong>My Sessions</strong>
-                    </Nav.Link>
-                    <Nav.Link onClick={Auth.logout}><strong>Logout</strong></Nav.Link>
-                  </>
-                ) : (
-                  <Nav.Link onClick={() => setShowModal(true)}>
-                    Login/Sign Up
-                  </Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
+                <Nav.Link onClick={Auth.logout}>
+                  <strong>Logout</strong>
+                </Nav.Link>
+              </>
+            ) : (
+              <Nav.Link onClick={() => setShowModal(true)}>
+                Login/Sign Up
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Modal
         size="lg"
